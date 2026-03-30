6d3a70cabbd279cb0845d2c3ced4f50e414067aa
meghanagottapu <meghanagottapu@gmail.com>
2026-03-30 01:33:27 -0400

    Dropdown column selectors for all transform forms

--- dataloom-frontend/src/Components/forms/FilterForm.jsx
+++ dataloom-frontend/src/Components/forms/FilterForm.jsx
@@ -5,6 +5,7 @@ import { FILTER } from "../../constants/operationTypes";
 import TransformResultPreview from "./TransformResultPreview";
 import useError from "../../hooks/useError";
 import FormErrorAlert from "../common/FormErrorAlert";
+import ColumnSelect from "../common/ColumnSelect";
 
 const FilterForm = ({ projectId, onClose }) => {
   const [filterParams, setFilterParams] = useState({
@@ -50,13 +51,11 @@ const FilterForm = ({ projectId, onClose }) => {
         <div className="flex flex-wrap mb-4">
           <div className="w-full sm:w-1/3 mb-2">
             <label className="block mb-1 text-sm font-medium text-gray-700">Column:</label>
-            <input
-              type="text"
+            <ColumnSelect
               name="column"
               value={filterParams.column}
               onChange={handleInputChange}
-              className="border border-gray-300 rounded-md px-3 py-2 w-full bg-white text-gray-900 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 focus:outline-none"
-              required
+              placeholder="Select column to filter..."
             />
           </div>
           <div className="w-full sm:w-1/3 mb-2 pl-2">
